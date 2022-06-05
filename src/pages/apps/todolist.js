9abd75101745078e1c7e8779ea0db10b000ee67c
edmundchan8 <pokedev8@gmail.com>
2022-06-05 10:28:09 -0700

    localstorage added to todo list app

--- src/pages/apps/todolist.js
+++ src/pages/apps/todolist.js
@@ -5,14 +5,29 @@ import Layout from '../../components/layout'
 function ToDoListPage (){
 
     const [todo, setTodo] = useState("");
-    const [list, setList] = useState([]);
+    let savedList = [];
+    if (JSON.parse(localStorage.getItem('list')) !== null){
+        savedList = JSON.parse(localStorage.getItem('list'));
+    }
+    const [list, setList] = useState(savedList);
+    
+//    localStorage.setItem("list", []);
 
     function handleChange(event){
         setTodo(event.target.value);
     }
 
     function handleClick(){
-        setList(prevValue => {return [...prevValue, todo]})
+        setList(prevValue => {return [...prevValue, todo]});
+        setTodo('');
+    }
+
+    function handleSave(){
+        localStorage.setItem("list", JSON.stringify(list));
+    }
+
+    function handleDelete(){
+        localStorage.clear("list");
     }
 
     return (
@@ -21,8 +36,11 @@ function ToDoListPage (){
             <input type="text" placeholder='Enter a todo' value={todo} onChange={handleChange}/>
             <button onClick={handleClick}>Add Todo</button>
             <ul>
-                {list.map(item => <li>{item}</li>)}
+                {list.map((item, index) => <li key={index}>{item}</li>)}
             </ul>
+            <button onClick={handleSave}>Save Data to Local Storage</button>
+            <button onClick={handleDelete}>Delete Local Storage Data</button>
+            
         </Layout>
     )
 }
